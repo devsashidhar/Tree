73009b24b081705786c6ccef816dfbd8040c5227
Dev Sashidhar <devsashidhar@gmail.com>
2024-10-10 21:14:38 -0400

    Initial Commit

---
+++ Tree/TreeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TreeApp.swift
+//  Tree
+//
+//  Created by Dev Sashidhar on 10/10/24.
+//
+
+import SwiftUI
+
+@main
+struct TreeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TreeTests/TreeTests.swift
@@ -0,0 +1,16 @@
+//
+//  TreeTests.swift
+//  TreeTests
+//
+//  Created by Dev Sashidhar on 10/10/24.
+//
+
+import Testing
+
+struct TreeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
